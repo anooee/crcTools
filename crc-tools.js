c1da317d659145d347ce7bab50c98f7498067ce3
anooee <anooee@qq.com>
2019-02-20 21:46:32 +0800

    Update crc-tools.js

--- crc-tools.js
+++ crc-tools.js
@@ -1,4 +1,13 @@
 /**
+ * 确切的说当前仅仅是CRC-16/CCITT-FALSE版本，通用版本在后续更新。
+ * 计算结果查考网站：http://www.ip33.com/crc.html
+ * 
+ * eg. strA = '你好'
+ *     strA的hex数组为 [0x4f,0x60,0x59,0x7d]
+ *     计算结果为 BEC4
+ */
+
+ /**
  * CRC（循环冗余校验）anooee
  * @param {Buffer} buffer HEX数组
  */
